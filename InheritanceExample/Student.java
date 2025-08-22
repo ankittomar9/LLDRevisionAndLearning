2a9eac701890212aa8eda6d78b1a92a0b49d2fa8
Ankit <tomarankit78@gmail.com>
2025-08-22 05:49:07 +0530

    LLD practice Problems

---
+++ InheritanceExample/Student.java
@@ -0,0 +1,30 @@
+class Student{
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', age=" + age + '}';
+    }
+
+
+
+    public static void main(String[] args) {
+        Student student = new Student("John Doe", 20);
+        System.out.println(student);
+    }
+
+}
